6d09745e6f1e2f91946bccdd735df3cfdf9718c8
icedancemelody <49614958+icedancemelody@users.noreply.github.com>
2020-05-26 21:06:21 +0800

    一言难尽加了些啥

--- src/components/studentHome/JoinnewActivity.vue
+++ src/components/studentHome/JoinnewActivity.vue
@@ -1,6 +1,24 @@
 <template>
     <div>
-        <h3>JoinnewActivity组件</h3>
+        <!--        面包屑 （导航区域)-->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/studentWelcome' }">学生首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/joinActivity' }">参加活动</el-breadcrumb-item>
+            <el-breadcrumb-item>参加新活动</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!--        卡片-->
+        <el-card class="box-card">
+            123
+            <!--            搜索与添加-->
+
+            <!--                    搜索取消时也会刷新搜索页面,搜索确定时,将携带query搜索特点内容的活动-->
+            <el-input placeholder="lyr is poisonous">
+
+            </el-input>
+        </el-card>
+
     </div>
 </template>
 
