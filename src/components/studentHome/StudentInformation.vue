5d6818ddc6564514c4e5d2982977879a6d651ba1
icedancemelody <49614958+icedancemelody@users.noreply.github.com>
2020-08-11 21:31:12 +0800

    照负责人的界面美化了一下

--- src/components/studentHome/StudentInformation.vue
+++ src/components/studentHome/StudentInformation.vue
@@ -1,20 +1,38 @@
 <template>
-<div>StudentInformation</div>
+    <div>
+        <!--        面包屑-->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item :to="{ path: '/principleWelcome' }">学生首页</el-breadcrumb-item>
+            <el-breadcrumb-item>个人信息管理</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <el-card class="box-card">
+            <!--            搜索与添加-->
+            <el-row :gutter="20">
+                <el-row :gutter="20">
+                    <el-col :span="7">
+                    </el-col>
+
+                </el-row>
+            </el-row>
+            <!--            竖着展示信息，对应一个按钮修改-->
+            <el-table :data="activityList">
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="姓名" prop="name"></el-table-column>
+                <el-table-column label="学号" prop="place"></el-table-column>
+
+            </el-table>
+
+        </el-card>
+
+    </div>
 </template>
 
+
 <script>
-    export default {
-        data(){
-            return{}
-        },
-        created()
-        {
-
-        },
-        methods:{
-
-        }
-    }
 
 </script>
+
 <style scoped></style>
+
