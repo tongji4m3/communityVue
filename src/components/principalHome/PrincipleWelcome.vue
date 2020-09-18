7a16c65b34e6d594e664187a689de9bae0897405
riobun <49815645+riobun@users.noreply.github.com>
2020-09-18 14:55:39 +0800

    负责人欢迎页面

--- src/components/principalHome/PrincipleWelcome.vue
+++ src/components/principalHome/PrincipleWelcome.vue
@@ -26,7 +26,7 @@
 
                     <el-table :data="AnnouncementList" height="300" :cell-style="{padding:'5px 0'}">
                         <el-table-column type="index" width="40"></el-table-column>
-                        <el-table-column label="标题" prop="title" width="630"></el-table-column>
+                        <el-table-column label="标题" prop="title" width="470%"></el-table-column>
                         <el-table-column label="系统公告时间" prop="time"></el-table-column>
                         <el-table-column label="显示详情">
                             <template slot-scope="scope">
